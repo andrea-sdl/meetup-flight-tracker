5fe8d1c4188fda1939df776f1646a3f8b4e9c746
Andrea Grassi <andrea.grassi@automattic.com>
2023-03-11 15:34:20 +0100

    Add some basic caching to avoid doing multiple calls for the same combination

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const readline = require('readline');
-const Amadeus = require('amadeus');
-const dotenv = require('dotenv');
+const fs = require("fs");
+const readline = require("readline");
+const Amadeus = require("amadeus");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -12,26 +12,37 @@ const RETURN_DATE = process.argv[5]; // Get return date from command-line argume
 
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_API_KEY,
-  clientSecret: process.env.AMADEUS_API_SECRET
+  clientSecret: process.env.AMADEUS_API_SECRET,
 });
 
-async function getAirfarePrice(origin) {
+async function getAirfarePrice(origin, pricesCache) {
+  if (pricesCache[origin] !== undefined) {
+    console.log(
+      `Using cached price from ${origin} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE}: $${pricesCache[origin]}`
+    );
+    return pricesCache[origin];
+  }
   try {
-    const response= await amadeus.shopping.flightOffersSearch.get({
+    const response = await amadeus.shopping.flightOffersSearch.get({
       originLocationCode: origin,
       destinationLocationCode: DESTINATION,
       departureDate: DEPARTURE_DATE,
       returnDate: RETURN_DATE,
       adults: 1,
-      currencyCode:'USD',
-      max:1
+      currencyCode: "USD",
+      max: 3,
     });
-    if (response?.data && response.data.length > 0 ){
+    if (response?.data && response.data.length > 0) {
       const price = response.data[0].price.total;
-      console.log(`Airfare price from ${origin} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE}: $${price}`);
-      return price-0;
+      console.log(
+        `Airfare price from ${origin} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE}: $${price}`
+      );
+      pricesCache[origin] = price - 0;
+      return pricesCache[origin];
     }
-    console.error(`[NOT-FOUND] Airfare price from ${origin} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE} could not be found`);
+    console.error(
+      `[NOT-FOUND] Airfare price from ${origin} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE} could not be found`
+    );
     return 0;
   } catch (error) {
     console.error(error);
@@ -42,17 +53,20 @@ async function getAirfarePrice(origin) {
 async function main() {
   let totalPrice = 0;
 
+  const pricesCache = {};
   const rl = readline.createInterface({
     input: fs.createReadStream(ORIGINS_FILE),
-    crlfDelay: Infinity
+    crlfDelay: Infinity,
   });
 
   for await (const origin of rl) {
-    const price = await getAirfarePrice(origin.trim());
+    const price = await getAirfarePrice(origin.trim(), pricesCache);
     totalPrice += price;
   }
 
-  console.log(`Total airfare price from all origins in ${ORIGINS_FILE} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE}: $${totalPrice}`);
+  console.log(
+    `Total airfare price from all origins in ${ORIGINS_FILE} to ${DESTINATION} on ${DEPARTURE_DATE} - ${RETURN_DATE}: $${totalPrice}`
+  );
 }
 
 main();
